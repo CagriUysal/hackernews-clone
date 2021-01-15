2832f338beabca3588452b50c33cd13f741b9f51
cagri <cagri.uysal07@gmail.com>
2021-01-15 21:09:10 +0300

    types added to resolver file

--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -1,23 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import { User, Post } from "@prisma/client/index";
 
 const prisma = new PrismaClient();
 
 export const resolvers = {
   MutationResponse: {
-    __resolveType: (response) => {
+    __resolveType: (response): string => {
       if (response.post) return "Post";
       if (response.user) return "User";
     },
   },
 
   Query: {
-    posts: async () => {
+    posts: async (): Promise<Post[]> => {
       return await prisma.post.findMany({ include: { author: true } });
     },
   },
 
   Mutation: {
-    addPost: async (_, { post: { link, title, userName } }) => {
+    addPost: async (
+      _,
+      {
+        post: { link, title, userName },
+      }: { post: { link: string; title: string; userName: string } }
+    ): Promise<IMutationResponse> => {
       const newPost = await prisma.post.create({
         data: {
           link,
@@ -37,7 +43,10 @@ export const resolvers = {
       };
     },
 
-    addUser: async (_, { user: { name } }) => {
+    addUser: async (
+      _,
+      { user: { name } }: { user: { name: string } }
+    ): Promise<IMutationResponse> => {
       const newUser = await prisma.user.create({
         data: { name },
       });
@@ -51,3 +60,11 @@ export const resolvers = {
     },
   },
 };
+
+interface IMutationResponse {
+  code: string;
+  success: boolean;
+  message: string;
+  post?: Post;
+  user?: User;
+}
