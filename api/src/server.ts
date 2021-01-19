e88d65035d4dd92aa562e555d36ce91f136457ba
cagri <cagri.uysal07@gmail.com>
2021-01-19 13:26:49 +0300

    `isAuth` function added to context.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,31 @@
 import { ApolloServer } from "apollo-server";
+import { verify } from "jsonwebtoken";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ res }) => ({ res }),
+  context: ({ res, req }) => ({
+    res,
+    req,
+    isAuth: () => {
+      const authHeader = req.headers["authorization"];
+
+      if (!authHeader) {
+        throw new Error("Not Auth.");
+      }
+
+      try {
+        const token = authHeader.split(" ")[1];
+        const payload = verify(token, process.env.ACCESS_TOKEN); // throws an error if not valid
+
+        return payload;
+      } catch (error) {
+        throw new Error("Not Auth.");
+      }
+    },
+  }),
 });
 
 server.listen().then(({ url }) => console.log(`Server running at ${url}`));
