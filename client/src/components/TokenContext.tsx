07c3fbbb673dd9a19eab17f9f00861a769e1ed52
cagri <cagri.uysal07@gmail.com>
2021-01-20 14:51:50 +0300

    `TokenContext` added.

---
+++ client/src/components/TokenContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, FunctionComponent, useReducer } from "react";
+
+const initState = "";
+
+const TokenContext = createContext({});
+
+export const CHANGE_TOKEN = "CHANGE_TOKEN";
+export const DELETE_TOKEN = "DELETE_TOKEN";
+
+const reducer = (state, action) => {
+  if (action.type === CHANGE_TOKEN) {
+    const { token: newToken } = action.payload;
+
+    return newToken;
+  }
+
+  if (action.type === DELETE_TOKEN) {
+    return "";
+  }
+
+  return state;
+};
+
+export const TokenProvider: FunctionComponent = ({ children }) => {
+  const [token, dispatch] = useReducer(reducer, initState);
+
+  return (
+    <TokenContext.Provider value={{ token, dispatch }}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
