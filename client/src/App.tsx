89ea436d960b722b7860503a6f2c83bca6a9fded
cagri <cagri.uysal07@gmail.com>
2021-01-20 23:48:27 +0300

    try to get access token in app mount

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import React, { FunctionComponent } from "react";
+import React, {
+  FunctionComponent,
+  useEffect,
+  useContext,
+  useState,
+} from "react";
 
 import { Router } from "@reach/router";
 import { Global, css } from "@emotion/react";
-import { ApolloProvider } from "@apollo/client";
 
-import { TokenProvider } from "./components/TokenContext";
-import client from "./client";
+import { TokenContext, CHANGE_TOKEN } from "./components/TokenContext";
 
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -18,6 +21,21 @@ import Submit from "./pages/Submit";
 import Login from "./pages/Login";
 
 const App: FunctionComponent = () => {
+  const { dispatch } = useContext(TokenContext);
+
+  useEffect(() => {
+    // prevent login in every refresh
+    fetch("http://localhost:3000/refresh", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const { accessToken } = data;
+        dispatch({ type: CHANGE_TOKEN, payload: { token: accessToken } });
+      });
+  }, []);
+
   return (
     <>
       <Global
@@ -40,21 +58,17 @@ const App: FunctionComponent = () => {
           }
         `}
       />
-      <ApolloProvider client={client}>
-        <TokenProvider>
-          <Router>
-            <Home path="/" />
-            <New path="/newest" />
-            <Past path="/front" />
-            <Comments path="/newcomments" />
-            <Ask path="/ask" />
-            <Show path="/show" />
-            <Jobs path="/jobs" />
-            <Submit path="/submit" />
-            <Login path="/login" />
-          </Router>
-        </TokenProvider>
-      </ApolloProvider>
+      <Router>
+        <Home path="/" />
+        <New path="/newest" />
+        <Past path="/front" />
+        <Comments path="/newcomments" />
+        <Ask path="/ask" />
+        <Show path="/show" />
+        <Jobs path="/jobs" />
+        <Submit path="/submit" />
+        <Login path="/login" />
+      </Router>
     </>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { ApolloProvider } from "@apollo/client";
+
+import { TokenProvider } from "./components/TokenContext";
+import client from "./client";
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <ApolloProvider client={client}>
+    <TokenProvider>
+      <App />
+    </TokenProvider>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
