7febddaf7670d62c788e4d20e067cd8c0ff7b602
cagri <cagri.uysal07@gmail.com>
2021-02-19 11:07:20 +0300

    logs cleared from pastPosts.

--- api/src/resolvers/pastPosts.ts
+++ api/src/resolvers/pastPosts.ts
@@ -6,9 +6,6 @@ export default async function pastPosts(
   _,
   { input: { start, end } }: { input: { start: string; end: string } }
 ): Promise<Post[]> {
-  console.log(start);
-  console.log(end);
-
   const startDate = new Date(start); // target date
   const endDate = new Date(end); // target day + 1 day, exclusive
 
